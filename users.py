e4b29d6936ba871f86f72ce17bb7b833b0114e79
kevinthedang <kevinthedang_1@outlook.com>
2022-04-15 15:42:08 -0700

    ChatUser now handles new properties

--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 import queue
 import logging
+from types import new_class
 from constants import *
 from datetime import date, datetime
 from pymongo import MongoClient
@@ -15,11 +16,14 @@ logging.basicConfig(filename='message_chat.log', level=logging.DEBUG, format = L
 class ChatUser():
     """ class for users of the chat system. Users must be registered 
     """
-    def __init__(self, alias: str, user_id = None, create_time: datetime = datetime.now(), modify_time: datetime = datetime.now()) -> None:
+    def __init__(self, alias: str, user_id = None, email: str = '', blacklist: list = [], create_time: datetime = datetime.now(), modify_time: datetime = datetime.now()) -> None:
         self.__alias = alias
         self.__user_id = user_id
+        self.__email = email
         self.__create_time = create_time
         self.__modify_time = modify_time
+        self.__hash_pass = ''
+        self.__blacklist = blacklist
         if self.__user_id is not None:
             self.__dirty = False
         else:
@@ -29,9 +33,29 @@ class ChatUser():
     def alias(self):
         return self.__alias
 
+    @alias.setter
+    def alias(self, new_alias):
+        if len(new_alias > 2):
+            self.__alias = new_alias
+            self.dirty = True
+
     @property
     def user_id(self):
         return self.__user_id
+
+    @user_id.setter
+    def user_id(self, new_id):
+        self.__user_id = new_id
+        self.dirty = True
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, new_email):
+        self.__email = new_email
+        self.dirty = True
     
     @property
     def dirty(self):
@@ -39,11 +63,50 @@ class ChatUser():
 
     @dirty.setter
     def dirty(self, new_value: bool):
+        self.__modify_time = datetime.now()
         self.__dirty = new_value
+
+    @property
+    def blacklist(self):
+        return self.__blacklist
+
+    @property
+    def hash_pass(self):
+        return self.__hash_pass
+
+    @hash_pass.setter
+    def hash_pass(self, new_password):
+        self.__hash_pass = new_password
+        self.dirty = True
+
+    def add_alias_to_blacklist(self, alias) -> bool:
+        ''' This method will add a user's alias to the current user's blacklist
+            given that the alias is not already in the users blacklist
+            NOTE: consider checking if the user exists at the beginning
+        '''
+        if alias not in self.blacklist:
+            self.blacklist.append(alias)
+            self.dirty = True
+            return True
+        return False
+
+    def remove_alias_from_blacklist(self, alias) -> bool:
+        ''' This method will remove a user's alias from the current user's blacklist
+            given that the alias is in the current user's blacklist
+            NOTE: consider checking if the alias exists as a user 
+                (may not be necessary since we just want to check if the alias 
+                exists in the blacklist, not as a user in the userlist)
+        '''
+        if alias in self.blacklist:
+            self.blacklist.remove(alias)
+            self.dirty = True
+            return True
+        return False
 
     def to_dict(self):
         return {
                 'alias': self.__alias,
+                'blacklist': self.blacklist,
                 'create_time': self.__create_time,
                 'modify_time': self.__modify_time
         }
@@ -61,6 +124,8 @@ class UserList():
             logging.info('UserList Document was found in the collection.')
             self.__dirty = False
         else:
+            self.__list_name = list_name
+            self.__id = None
             self.__create_time = datetime.now()
             self.__modify_time = datetime.now()
             self.__dirty = True
@@ -74,6 +139,31 @@ class UserList():
     @property
     def user_aliases(self):
         return self.get_all_users_aliases
+
+    # property to return the name of the user list
+    @property
+    def name(self):
+        return self.__list_name
+
+    # setter to set the name of the list
+    @name.setter
+    def name(self, new_list_name):
+        if len(new_list_name) > 2:
+            self.__list_name = new_list_name
+            self.__dirty = True
+
+    # property that checks if the userlist is dirty (needs persistance)
+    @property
+    def dirty(self):
+        return self.__dirty
+
+    # setter that sets the value of the userlist when the userlist is modified outside of userlist
+    @dirty.setter
+    def dirty(self, new_value):
+        if type(new_value) is bool:
+            self.__dirty = new_value
+            if new_value is True:
+                self.__modify_time = datetime.now()
     
     def register(self, new_alias: str) -> ChatUser:
         """ This method will just return a new ChatUser that will need to be added to the UserList
@@ -83,6 +173,18 @@ class UserList():
         if self.get(new_alias) is None:
             logging.debug(f'Registered new user with name {new_alias}.')
             return ChatUser(alias = new_alias)
+
+    def deregister(self, alias_to_remove: str) -> ChatUser:
+        ''' This method will return the chat user that was removed from the userlist
+            TODO: must remove the user from all members lists (all rooms)
+            NOTE: may want to remove this user from all of the users blacklist, 
+                or handle it in the restore, making sure that the user exists when updating the blacklist
+        '''
+        if (user := self.get(alias_to_remove)) is None:
+            return user
+        #user.
+        return user
+
 
     def get(self, target_alias: str) -> ChatUser:
         ''' This method will return the user from the user_list
@@ -124,14 +226,15 @@ class UserList():
             NOTE: we may not need the user aliases since we just want to restore all of the users            
         """
         logging.info(f'Attempting to restore user list metadata from {self.__list_name}.')
-        queue_metadata = self.__mongo_collection.find_one( { 'list_name': self.__list_name })
-        if queue_metadata is None:
+        user_list_metadata = self.__mongo_collection.find_one( { 'list_name': self.__list_name })
+        if user_list_metadata is None:
             logging.debug(f'{self.__list_name} user list was not found in the mongo collection.')
             return False
-        self.__list_name = queue_metadata['list_name']
-        self.__create_time = queue_metadata['create_time']
-        self.__modify_time = queue_metadata['modify_time']
-        self.__user_aliases = queue_metadata['user_names']
+        self.__list_name = user_list_metadata['list_name']
+        self.__id = user_list_metadata['_id']
+        self.__create_time = user_list_metadata['create_time']
+        self.__modify_time = user_list_metadata['modify_time']
+        self.__user_aliases = user_list_metadata['user_names']
         logging.info(f'Attempting to restore users to the {self.__list_name} list.')
         for current_user_alias in self.__user_aliases:
             current_user_metadata = self.__mongo_collection.find_one({ 'alias' : current_user_alias })
